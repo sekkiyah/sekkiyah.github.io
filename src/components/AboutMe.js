88a9d5aa7072aab6c214e65376050aafa9207247
sekkiyah <seth_hinshaw@hotmail.com>
2023-10-15 01:16:35 -0500

    folder structure updated and logos added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 
 const AboutMe = () => {
   return (
-    <>
+    <Container className='page-segment'>
       <h1 className='text-center'>Hello! I'm Seth</h1>
 
       <h1 className='text-center'>About Me</h1>
-    </>
+      <Button onClick={() => window?.open('/files/seth-resume-2023.v1.pdf', '_blank')}>Download CV</Button>
+    </Container>
   );
 };
 
