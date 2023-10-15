773c547a8c5112665538c9a9e7016010aed8e22d
sekkiyah <seth_hinshaw@hotmail.com>
2023-10-14 22:48:06 -0500

    added navbar and initial components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { NavigationBar } from './';
 
 const Home = () => {
   return (
     <>
-      <h1>Portfolio Site</h1>
-      <p>Testing</p>
+      <NavigationBar />
+      <h1 className='text-center'>Coming soon</h1>
     </>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Home } from './Home';
+export { default as NavigationBar } from './NavigationBar';
+export { default as AboutMe } from './AboutMe';
+export { default as Projects } from './Projects';
+export { default as Resume } from './Resume';
+export { default as Contact } from './Contact';
